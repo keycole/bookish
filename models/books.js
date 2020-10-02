2e790121db2bf853c2298cadee1873002b40f225
Nicole Graiff <nicole.graiff@gmail.com>
2020-10-02 10:43:35 -0400

    Set up preliminary books mongoose model.

---
+++ models/books.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: { type: Array, required: true },
+  publisher: String,
+  publishedDate: String,
+  pageCount: Number,
+  description: String,
+  image: String,
+  link: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
